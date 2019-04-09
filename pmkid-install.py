53742e26637ac5b41b90bfa7f18cf391b20a46f1
Zian25 <ziandv@gmail.com>
2019-04-09 20:18:50 -0300

    Update pmkid-install.py

--- pmkid-install.py
+++ pmkid-install.py
@@ -6,8 +6,8 @@ print "Começando..."
 os.system("cd /root/");
 os.system("apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get install libssl-dev && apt-get install libz-dev && apt-get install libpcap-dev && apt-get install libcurl4-openssl-dev")
 os.system("mkdir background");
-print "Baixando" 
 os.system("clear")
+print "Baixando" 
 os.system("git clone https://github.com/ZerBea/hcxdumptool.git");
 os.system("git clone https://github.com/ZerBea/hcxtools");
 os.system("mv hcxdumptool /root/background && cd /root/background/hcxdumptool && make && make install");
