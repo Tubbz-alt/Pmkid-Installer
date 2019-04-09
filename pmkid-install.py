f5ed04ff343fec957710aa87e7b7ac85cd6a4063
root <root@localhost.localdomain>
2019-04-09 20:14:27 -0300

    Instalador

---
+++ pmkid-install.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+#!/bin/bash
+import os, sys, time
+
+print "Começando..."
+os.system("cd /root/");
+os.system("apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get install libssl-dev && apt-get install libz-dev && apt-get install libpcap-dev && apt-get install libcurl4-openssl-dev")
+os.system("mkdir background");
+print "Baixando" 
+os.system("clear")
+os.system("git clone https://github.com/ZerBea/hcxdumptool.git");
+os.system("git clone https://github.com/ZerBea/hcxtools");
+os.system("mv hcxdumptool /root/background && cd /root/background/hcxdumptool && make && make install");
+os.system("mv hcxtools /root/background && cd /root/background/hcxtools && make && make install");
+
+
+
