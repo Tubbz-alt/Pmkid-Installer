0ba6d7334603fd5bec147f44ac54d0de10d7bd5f
Zian25 <ziandv@gmail.com>
2019-05-01 18:05:54 -0300

    Update pmkid-install.py

--- pmkid-install.py
+++ pmkid-install.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 #!/bin/bash
-import os, sys
+import os
 
 print "Começando..."
 os.system("apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get install libssl-dev && apt-get install libz-dev && apt-get install libpcap-dev && apt-get install libcurl4-openssl-dev")
