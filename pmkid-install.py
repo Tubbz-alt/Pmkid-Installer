3eec1d6ce6c156e2fb486fc0ba16763edc8ab646
Zian25 <ziandv@gmail.com>
2020-03-29 06:32:28 -0300

    Atualizando para Python 3

--- pmkid-install.py
+++ pmkid-install.py
@@ -2,16 +2,18 @@
 #!/bin/bash
 import os
 
-print "Começando..."
+print ("Começando...")
 os.system("apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get install libssl-dev && apt-get install libz-dev && apt-get install libpcap-dev && apt-get install libcurl4-openssl-dev")
 os.system("clear")
-print "Baixando" 
+print ("Baixando hcx's...") 
 os.system("git clone https://github.com/ZerBea/hcxdumptool.git");
 os.system("git clone https://github.com/ZerBea/hcxtools");
 os.system("cd hcxdumptool && make && make install");
+print("Making hcxdumptool...")
 os.system("cd ..")
 os.system("cd hcxtools && make && make install");
+print("Making hxctools...")
 os.system("clear")
-print "Concluido"
+print ("Concluido!")
 
 
